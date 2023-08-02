ac02f874d7c553f9f081dc6a0864f69b7325228e
DragonTs <106637358+Dragon-ts@users.noreply.github.com>
2023-08-02 12:25:32 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -3,15 +3,20 @@ import yaml
 from functions import on_startswith_options, on_fullmatch_options
 from switch_case_decorator import SwitchCaseDecorator
 from botpy.types.message import Message
-import var
+from var import Event
 
 
 class MyClient(botpy.Client):
     async def on_at_message_create(self, message: Message):
         talk = message.content
-        var.channel_id = message.channel_id
-        var.client = self
-        talk = str(talk).replace(f'<@!{self.robot.id}> ', '')  # 去除@bot前缀
+        Event.message = message
+        print(message)
+        Event.channel_id = message.channel_id
+        Event.user_id = message.author.id
+        Event.message_id = message.id
+        Event.client = self
+        talk = str(talk).replace(f'<@!{self.robot.id}> ', '').rstrip()  # 去除@bot前缀
+        print(talk)
         decorator = SwitchCaseDecorator(on_startswith_options, on_fullmatch_options)
         await decorator.match_sequence(talk)
 
@@ -20,6 +25,6 @@ with open('config.yml', 'r', encoding='utf-8') as yml:
     yml = yaml.load(yml, yaml.FullLoader)['account']
     appid = yml['app-id']
     token = yml['token']
-client = MyClient(intents=botpy.Intents(public_guild_messages=True))
+client = MyClient(intents=botpy.Intents.all())
 client.run(appid=appid, token=token)
 print(123)
