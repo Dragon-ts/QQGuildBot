aa5c461f7b102df656046e0c91a2aa957c272bd4
DragonTs <106637358+Dragon-ts@users.noreply.github.com>
2023-08-02 12:32:57 +0800

    Update command.py
    示例指令

--- command.py
+++ command.py
@@ -1,18 +1,14 @@
-import logging
-import botpy.message
 from functions import on_fullmatch, on_startswith
 import var
 
 
-@on_fullmatch('nya')
-async def func1():
-    print('cool')
-    await var.client.api.post_message(channel_id=var.channel_id, content='test finished! Nya!')
+@on_fullmatch('hello')  # 当消息只是hello时执行下边的函数
+async def hello():
+    await var.send('hello')  # 发送普通信息
+    await var.MessageSegment.reply('hello')  # 发送回复信息
+    await var.MessageSegment.at('hello')  # 发送@信息
 
 
-@on_startswith('/')
-async def func2():
-    print('')
-    with open(r'C:\Users\DELL\Desktop\00000\123456.txt', 'w') as f:
-        f.write('cool')
-
+@on_startswith('good')  # 当消息开头为good时执行下边的函数
+async def good():
+    await var.send(var.Event.channel_id)  # Event内置了一些消息变量可供使用
